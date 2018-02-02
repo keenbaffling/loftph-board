8978b92a169f0d1b8e84ccade7d7bceaed81e8b5
Klinton Ballecer <klinton.ballecer@gmail.com>
2018-02-02 16:04:03 +0800

    :hammer: Add user message formatting

--- client/src/components/Slack/index.js
+++ client/src/components/Slack/index.js
@@ -1,37 +1,34 @@
 import React, { Component } from 'react';
 import Slack from 'slack';
-// import axios from 'axios';
+import axios from 'axios';
 import _ from 'lodash';
-// import WebSocket from 'ws';
+import moment from 'moment';
 
-import List from './List';
+// import List from './List';
 
 const SLACK_BOT_TOKEN = process.env.REACT_APP_SLACK_BOT_TOKEN;
-const SLACK_CLIENT_ID = process.env.REACT_APP_SLACK_CLIENT_ID;
-const SLACK_CLIENT_SECRET = process.env.REACT_APP_SLACK_CLIENT_SECRET;
+// const SLACK_CLIENT_ID = process.env.REACT_APP_SLACK_CLIENT_ID;
+// const SLACK_CLIENT_SECRET = process.env.REACT_APP_SLACK_CLIENT_SECRET;
 const SLACK_CHANNEL = process.env.REACT_APP_SLACK_CHANNEL;
 
 const bot = new Slack({ token: SLACK_BOT_TOKEN });
 
 export default class extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      url: '',
-      history: [],
-      channel: [],
-      members: [],
-      emoji: [],
-      message: []
-    };
-  }
+  state = {
+    isLoading: true,
+    url: '',
+    history: null,
+    channel: null,
+    members: null,
+    emoji: null,
+    message: null
+  };
 
   componentDidMount() {
     this.rtmConnect();
     // this.rtmStart();
     this.getHistory(SLACK_CHANNEL);
-    this.getChannelInfo(SLACK_CHANNEL);
+    // this.getChannelInfo(SLACK_CHANNEL);
     this.getUsersList();
     this.getEmoji();
   }
@@ -39,9 +36,10 @@ export default class extends Component {
   getHistory = channel => {
     bot.channels
       .history({
-        channel
+        channel,
+        count: 30
       })
-      .then(res => _.reverse(res.messages,))
+      .then(res => res.messages)
       .then(history => this.setState({ history }))
       .catch(console.error);
   };
@@ -57,8 +55,7 @@ export default class extends Component {
   };
 
   getUsersList = () => {
-    bot
-      .users
+    bot.users
       .list()
       .then(res => res.members)
       .then(members => this.setState({ members }))
@@ -66,8 +63,7 @@ export default class extends Component {
   };
 
   getEmoji = () => {
-    bot
-      .emoji
+    bot.emoji
       .list()
       .then(res => res.emoji)
       .then(emoji => this.setState({ emoji }))
@@ -75,57 +71,133 @@ export default class extends Component {
   };
 
   rtmConnect = () => {
-    bot
-      .rtm
+    bot.rtm
       .connect()
-      // .then(console.log)
       .then(res => this.setState({ url: res.url }))
       .then(this.handleRTM)
       .catch(console.error);
   };
 
   rtmStart = () => {
-    bot
-      .rtm
+    bot.rtm
       .start()
-      // .then(console.log)
       .then(res => this.setState({ url: res.url }))
       .then(this.handleRTM)
       .catch(console.error);
   };
 
-  handleRTM = () => {
-    // const WebSocket = require('ws');
-    const ws = new WebSocket(this.state.url);
+  getUser = userId => _.filter(this.state.members, ['id', userId])[0];
 
+  handleUserTag = userTag => {
+    let str = '';
+    let user = this.getUser(userTag.substr(2, 9));
+
+    if (user) {
+      str = `<a className="slack__text-highlight" href="https://loftph.slack.com/team/${
+        user.id
+      }" target="_blank">@${user.profile.display_name}</a>`;
+    }
+
+    return str;
+  };
+
+  formatMessage = text => {
+    text = text.toString();
+    let newText = '';
+    let taggedUsers = text.match(/<@\w+>/g);
+
+    if (taggedUsers && taggedUsers.length) {
+      // Replace user tags
+      _.forEach(taggedUsers, (user, index) => {
+        if (newText === '') {
+          newText = text.replace(user, this.handleUserTag(user));
+        } else {
+          newText = newText.replace(user, this.handleUserTag(user));
+        }
+      });
+    } else {
+      newText = text;
+    }
+
+    // Replace img
+
+    return newText;
+  };
+
+  handleRTM = () => {
+    const ws = new WebSocket(this.state.url);
 
     ws.onmessage = event => {
       const data = JSON.parse(event.data);
+      const { history } = this.state;
 
       switch (data.type) {
         case 'message':
-          if (data.channel === SLACK_CHANNEL) {
-            this.setState({ history: this.state.history.concat(data) });
+          if (data.channel === SLACK_CHANNEL && !!data.subtype === false) {
+            history.unshift(data);
+            this.setState({ history });
           }
           break;
 
         default:
           break;
       }
+
+      this.setState({ isLoading: false });
     };
   };
 
   render() {
-    const { history } = this.state;
+    const { isLoading, history } = this.state;
+
+    if (isLoading) {
+      return <div>Loading...</div>;
+    }
 
     return (
       <React.Fragment>
-        {/* Loading... */}
-        {/* <List messages={messages} /> */}
+        {history &&
+          history.map((item, index) => {
+            let user = this.getUser(item.user);
+            let profile = user && user.profile;
+            let timestamp = new Date(item.ts * 1000).toLocaleString();
 
-        {!!history.length && history.map((item, index) => (
-          <li key={index}>{item.user} | {item.text}</li>
-        ))}
+            return (
+              <div className="slack__message" key={index}>
+                <div className="slack__wrap">
+                  <div className="slack__gutter">
+                    <a href="/" className="slack__avatar">
+                      <img
+                        className="slac__avatar-img"
+                        src={profile.image_192}
+                        alt={profile.display_name}
+                      />
+                    </a>
+                  </div>
+                  <div className="slack__message-content">
+                    <div className="slack__message-header">
+                      <span className="slack__message-sender">
+                        <a href="/" className="slack__message-sender-link">
+                          {profile.display_name}
+                        </a>
+                      </span>
+                      <span href="#" className="slack__timestamp">
+                        <span className="slack__timestamp-label">
+                          {moment(timestamp).format('LT')}
+                        </span>
+                      </span>
+                    </div>
+                    <div
+                      className="slack__message-body"
+                      dangerouslySetInnerHTML={{
+                        __html: this.formatMessage(item.text)
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
+            );
+          })}
       </React.Fragment>
     );
   }
